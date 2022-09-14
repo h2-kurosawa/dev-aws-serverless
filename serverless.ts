29ca3d9bac83ec305230a49863f71ed957537da9
h2-kurosawa <h2-kurosawa@prime-x.co.jp>
2022-09-14 10:29:00 +0900

    change name

--- serverless.ts
+++ serverless.ts
@@ -3,7 +3,7 @@ import type { AWS } from '@serverless/typescript';
 import hello from '@functions/hello';
 
 const serverlessConfiguration: AWS = {
-  service: 'my-ts-app',
+  service: 'dev-sls',
   frameworkVersion: '3',
   plugins: ['serverless-esbuild'],
   provider: {
